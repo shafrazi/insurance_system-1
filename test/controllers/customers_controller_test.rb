489bb816a78abf5d8ebe221234016ff29a9bc4c7
Shafrazi Shiffa <20836739+shafrazi@users.noreply.github.com>
2022-10-22 14:22:11 +0530

    Update customers_controller_test.rb
    change test for customer controller

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -23,10 +23,7 @@ class CustomersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to customer_url(Customer.last)
   end
 
-  test "should show customer" do
-    get customer_url(@customer)
-    assert_response :success
-  end
+ 
 
   test "should get edit" do
     get edit_customer_url(@customer)
