978ecf2ca36c8a665f210607e69c0a2511ae9521
Shafrazi Shiffa <20836739+shafrazi@users.noreply.github.com>
2022-10-22 14:20:59 +0530

    Update customer.rb
    remove validation for address

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
   has_many :insurance_policies, dependent: :destroy, inverse_of: :customer
   accepts_nested_attributes_for :insurance_policies, reject_if: :all_blank, allow_destroy: true
   validates :name, presence: true
-  validates :address, presence: true
+  
 
   def self.search(query)
     where("name like ?", "%#{query}%")
